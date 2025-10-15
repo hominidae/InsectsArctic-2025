6e887fb61fa9b66245ab1b74cb9d6a2b88e7c703
Bryan Vandenbrink <bryan.vandenbrink@gmail.com>
2025-10-15 17:44:06 -0600

    Create 001_LoadData.R
    Initial commit for 001_LoadData.R

---
+++ 001_LoadData.R
@@ -0,0 +1,27 @@
+# Individual data analysis
+# Load the mBRAVE data and begin analysis.
+
+# Load the libraries
+library(tidyverse)
+
+# Load the tsv files
+ARCB22E <- read_tsv("mBRAVE data/MBR-ARCB22E.tsv")
+ARCBIO19A <- read_tsv("mBRAVE data/MBR-ARCBIO19A.tsv")
+ARCBIO19B <- read_tsv("mBRAVE data/MBR-ARCBIO19B.tsv")
+ARCBIO19C <- read_tsv("mBRAVE data/MBR-ARCBIO19C.tsv")
+ARCBIO19D <- read_tsv("mBRAVE data/MBR-ARCBIO19D.tsv")
+ARCBIO19E <- read_tsv("mBRAVE data/MBR-ARCBIO19E.tsv")
+ARCBIO19G <- read_tsv("mBRAVE data/MBR-ARCBIO19G.tsv")
+ARCBIO19H <- read_tsv("mBRAVE data/MBR-ARCBIO19H.tsv")
+ARCBIO20A <- read_tsv("mBRAVE data/MBR-ARCBIO20A.tsv")
+ARCBIO20B <- read_tsv("mBRAVE data/MBR-ARCBIO20B.tsv")
+ARCBIO20C <- read_tsv("mBRAVE data/MBR-ARCBIO20C.tsv")
+ARCBIO20D <- read_tsv("mBRAVE data/MBR-ARCBIO20D.tsv")
+ARCBIO20E <- read_tsv("mBRAVE data/MBR-ARCBIO20E.tsv")
+ARCBIO21A <- read_tsv("mBRAVE data/MBR-ARCBIO21A.tsv")
+ARCBIO22A <- read_tsv("mBRAVE data/MBR-ARCBIO22A.tsv")
+ARCBIOF <- read_tsv("mBRAVE data/MBR-ARCBIOF.tsv")
+MBR1 <- read_tsv("mBRAVE data/MBR-BBYTMT.tsv")
+MBR2 <- read_tsv("mBRAVE data/MBR-NSEQ00044.tsv")
+MBR3 <- read_tsv("mBRAVE data/MBR-NSEQ00091.tsv")
+MBR4 <- read_tsv("mBRAVE data/MBR-SCYTMT.tsv")
